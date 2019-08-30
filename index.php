b77f5735de4f826778c8b27aff6f3e33b07ec15c
Criatividade377 <54589614+Criatividade377@users.noreply.github.com>
2019-08-30 10:53:15 -0300

    traduzi o texto

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
 
-<<<<<<< HEAD
-  echo"Olá Mudndo 456!"
-
-  echo"Olá Kat"
-=======
-  echo"Olá Mundo 123"
->>>>>>> 4d39bc4c04eb079e22253d67c384526f756ed59e
-
-  echo"Olá Sei la
+  echo"Ola mundo!";
   ?>
